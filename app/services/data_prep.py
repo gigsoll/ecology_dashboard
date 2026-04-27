a013b604417ffba0acef629cb84f568dc12a634e
gigsoll <gigsollt@gmail.com>
2026-04-27 14:39:20 +0300

    feat: add web scraper

---
+++ app/services/data_prep.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+def scrape_data() -> None:
+    res = requests.get("https://opendata.gov.ua/dataset/air_monitor")
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    # Select only names of the files
+    regex: str = r"\d{4}-\d{2}-\d{2}"
+
+    for item in soup.find_all("div", {"class": "resource-item"}):
+        name_block = item.find("div", {"class": "data-resource-name-content"})
+        link_tag = name_block.find("a") if name_block else None
+
+        if not link_tag:
+            continue
+
+        table_name_raw = link_tag.string or ""
+        matches = re.findall(regex, table_name_raw)
+        if not matches:
+            continue
+
+        table_name = f"{matches[0]}.csv"
+
+        download_tag = item.find("a", {"class": "data-resource-download"}, href=True)
+        if not download_tag:
+            continue
+
+        table_link = download_tag.get("href")
+
+        file_downloaded = requests.get(table_link)
+        print(file_downloaded.status_code, table_name, table_link)
+
+        if file_downloaded.status_code == 200:
+            with open(table_name, "wb") as file:
+                file.write(file_downloaded.content)
